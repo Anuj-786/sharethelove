4b6e7e7b73e3fcfd24b5cd71b51e82201c886098
kapils003 <kapils003@gmail.com>
2016-03-30 15:58:47 +0530

    header menu has been added

--- wp-content/themes/sharethelove/header.php
+++ wp-content/themes/sharethelove/header.php
@@ -93,14 +93,24 @@
                                 <?php endif; ?>
                             </div><!-- .site-header-menu -->
                         <?php endif; ?>
-                        <?php if (is_user_logged_in()){                                    }
-                        else {
-                            print('<div class="log-in"><div class="login-trigger">Login</div>');
-                            print('<div class="login-content">'); echo do_shortcode('[my-login-form]');
-                            print('</div></div>');
-                        };
-
-                        ?>
+                        <?php 
+							if (is_user_logged_in()){
+									print('<a href="#menu" id="toggle"><span></span></a>
+										<div id="menu-exp">
+										  <ul>
+										    <li><a href="#home">Settings</a></li>
+										    <li><a href="#about">About</a></li>
+										    <li><a href="#support">Support Us</a></li>
+										    <li><a href="#faqs">FAQs</a></li>
+										    <li><a href="'); echo wp_logout_url(get_bloginfo('url')); print('" title="Logout">Logout</a>
+										  </ul>
+										</div>');								}
+									else {
+									    print('<div><div class="login-trigger">Login</div>');
+									    print('<div class="login-content">'); echo do_shortcode('[my-login-form]'); 
+									    print('</div></div>');
+									};
+						?>
                     </div><!-- .site-header-main -->
                 </header><!-- .site-header -->
 
