d4ccdc81e2611a2b599624e1c4222395ea571b58
ANUJ <anuj@wksanuj.(none)>
2016-04-01 11:06:11 +0530

    added a toggle menu for login and logout

--- wp-content/themes/sharethelove/header.php
+++ wp-content/themes/sharethelove/header.php
@@ -16,19 +16,19 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
-	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="site">
-	<div class="site-inner">
-		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
+	<div id="page" class="site">
+		<div class="site-inner">
+			<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
 
-		<header id="masthead" class="site-header" role="banner">
-			<?php if ( get_header_image() ) : ?>
-				<?php
+			<header id="masthead" class="site-header" role="banner">
+				<?php if ( get_header_image() ) : ?>
+					<?php
 					/**
 					 * Filter the default twentysixteen custom header sizes attribute.
 					 *
@@ -39,23 +39,23 @@
 					 * (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px'.
 					 */
 					$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
-				?>
-				<div class="header-image">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>">
-					</a>
-				</div><!-- .header-image -->
-			<?php endif; // End header image check. ?>
-			<div class="site-header-main">
-				<div class="site-branding">
-					<?php if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php endif;
+					?>
+					<div class="header-image">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+							<img src="<?php header_image(); ?>">
+						</a>
+					</div><!-- .header-image -->
+				<?php endif; // End header image check. ?>
+				<div class="site-header-main">
+					<div class="site-branding">
+						<?php if ( is_front_page() && is_home() ) : ?>
+							<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php else : ?>
+							<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php endif;
 
-					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
+						$description = get_bloginfo( 'description', 'display' );
+						if ( $description || is_customize_preview() ) : ?>
 						<p class="site-description"><?php echo $description; ?></p>
 					<?php endif; ?>
 				</div><!-- .site-branding -->
@@ -66,46 +66,48 @@
 					<div id="site-header-menu" class="site-header-menu">
 						<?php if ( has_nav_menu( 'primary' ) ) : ?>
 							<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Primary Menu', 'twentysixteen' ); ?>">
-							<?php
-									wp_nav_menu( array(
-										'theme_location' => 'primary',
-										'menu_class'     => 'primary-menu',
-									 ) );
-								?>
-							</nav><!-- .main-navigation -->
-						<?php endif; ?>
-
-						<?php if ( has_nav_menu( 'social' ) ) : ?>
-							<nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
 								<?php
+								wp_nav_menu( array(
+									'theme_location' => 'primary',
+									'menu_class'     => 'primary-menu',
+									) );
+									?>
+								</nav><!-- .main-navigation -->
+							<?php endif; ?>
+
+							<?php if ( has_nav_menu( 'social' ) ) : ?>
+								<nav id="social-navigation" class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Social Links Menu', 'twentysixteen' ); ?>">
+									<?php
 									wp_nav_menu( array(
 										'theme_location' => 'social',
 										'menu_class'     => 'social-links-menu',
 										'depth'          => 1,
 										'link_before'    => '<span class="screen-reader-text">',
 										'link_after'     => '</span>',
-									) );
-								?>
-							</nav><!-- .social-navigation -->
+										) );
+										?>
+									</nav><!-- .social-navigation -->
+								<?php endif; ?>
+							</div><!-- .site-header-menu -->
 						<?php endif; ?>
-					</div><!-- .site-header-menu -->
-				<?php endif; ?>
-			</div><!-- .site-header-main -->
-		</header><!-- .site-header -->
-		<?php
-		if (is_user_logged_in()){
-								print(' <div class="box-shadow-menu"></div>
-									  <ul class="login-menu">
-									    <li><a href="#home">Settings</a></li>
-									    <li><a href="#about">About</a></li>
-									    <li><a href="#support">Support Us</a></li>
-									    <li><a href="#faqs">FAQs</a></li>
-									    <li><a href="'); echo wp_logout_url(get_bloginfo('url')); print('" title="Logout">Logout</a>
-									  </ul>
-									</div>');								}
-								else {
-								    print('<div><div class="login-trigger">Login</div>');
-								    print('<div class="login-content">'); echo do_shortcode('[my-login-form]'); 
-								    print('</div></div>');
-								}; ?>
-		<div id="content" class="site-content">
+					</div><!-- .site-header-main -->
+					<div class="stl_login stl_logout">
+						<?php
+						if (is_user_logged_in()){
+							print(' <div class="box-shadow-menu"></div>
+								<ul class="login-menu">
+									<li><a href="#home">Settings</a></li>
+									<li><a href="#about">About</a></li>
+									<li><a href="#support">Support Us</a></li>
+									<li><a href="#faqs">FAQs</a></li>
+									<li><a href="'); echo wp_logout_url(get_bloginfo('url')); print('" title="Logout">Logout</a>
+									</ul>
+								</div>');								}
+									else {
+										print('<div><div class="login-trigger">Login</div>');
+										print('<div class="login-content">'); echo do_shortcode('[my-login-form]'); 
+										print('</div></div>');
+									}; ?>
+								</div>
+							</header><!-- .site-header -->
+							<div id="content" class="site-content">
