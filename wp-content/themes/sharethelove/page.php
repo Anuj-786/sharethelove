4d856433a8bc0222a818e7898bdd3bf0647eab4c
onkararun <aryamaaru@gmail.com>
2016-04-04 16:53:18 +0530

    add icons link in page,activity,userprofile.php file

--- wp-content/themes/sharethelove/page.php
+++ wp-content/themes/sharethelove/page.php
@@ -11,8 +11,21 @@
  * @since Twenty Sixteen 1.0
  */
 
-get_header(); ?>
 
+get_header(); ?>
+<div class="secondary-menu">
+<?php
+if (is_user_logged_in()) {
+print(' <div class="secondary-menu-logo">
+    <ul>
+    <li><a href="');echo home_url(); print('/?page_id=35"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/downbtn.png"></a></li>
+    <li><a href="');echo home_url(); print('/?page_id=286"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/downbtn.png"></a></li>
+    <li><a href="');echo home_url(); print('/?page_id=24"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/downbtn.png"></a></li>
+    <li><a href="');echo home_url(); print('/?page_id=104"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/downbtn.png"></a></li>
+	</ul>
+	</div>'); }
+?>
+</div>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<?php
