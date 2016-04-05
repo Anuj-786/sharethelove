f9851bbe8169d1c834a8a3e7a2d1b7f9ee9c7d6e
KAPIL <kapil@gaiwks.(none)>
2016-04-05 10:28:44 +0530

    changed the activity page and addded search page with project listing

--- wp-content/themes/sharethelove/activity.php
+++ wp-content/themes/sharethelove/activity.php
@@ -8,52 +8,67 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<?php 
-			$form_id = 5;
-			$entries = GFAPI::get_entries( $form_id );
-				foreach ($entries as $entry) {
-					$user_id = get_current_user_id();
-					$id = $entry[created_by];
-					$user_info = get_userdata($id);
-					if ($user_id == $id) {
-						$project = " ";
-						$project .= "<div class='header_image'> <img src=" .$entry[23]. "> </div>";
-						$project .= "<p>". $entry[1] .  "</p>";
-						$project .= "<p>Role Title: ". $entry[2] .  "</p>";
-						$project .= "<p>Vacancies: ". $entry[8] .  "</p>";
-						$project .= "<p>Posted By: " . $user_info->user_login . " Location: " . $entry[3] . "</p>";
-						$project .= "<p>Start Date: " . $entry[6] . " Ends On: " . $entry[7] . "</p>";
-						$project .= "<p>". $entry[4] .  "</p>";
-						$project .= "<img src=" .$entry[15]. ">";
-						$project .= "<img src=" .$entry[19]. ">";
-						$project .= "<img src=" .$entry[20]. ">";
-						$project .= "<img src=" .$entry[21]. ">";
-						$project .= "<div class='toggle-container'>";
-						$project .= "<p><h3>Project Description</h3>". $entry[10] .  "</p>";
-						$project .= "<p><h3>Role Description</h3>". $entry[11] .  "</p>";
-						$facility_exists = 0;
-						$facility = '';
-						foreach ($entry as $k => $val) {
-							if (strpos($k, '9.') !== FALSE && $val != '') {
-							  $facility_exists = 1;
-							  $facility .= "<li>" . $val . "</li>";
-						    }
-						}
-						if ($facility_exists) {
-							$project .= "<p><h3>Facilities:</h3>";
-							$project .= "<ul>" . $facility . "</ul></p>";
-						}
-						$project .= "<p><h3>Contact Details</h3>". $entry[12] .  "</p>";
-						$project .= "<p>" .$entry[13]. " " .$entry[14]. "</p>";
-						$project .= "</div>";
-						$project .= "<a class='moreshow'><span>Know More</span></a>";
-						//echo $project;
-						echo $project;
-					}else{
-						echo "";
-					}	
+            //get your custom posts ids as an array
+			$posts = get_posts(array(
+			    'post_type'   => 'projects',
+			    'post_status' => 'publish',
+			    'posts_per_page' => -1,
+			    'fields' => 'ids'
+			    )
+			);
+			//loop over each post
+			foreach($posts as $p){
+			    //get the meta you need from each post
+			    $pro_h_img = get_the_post_thumbnail($p);
+			    $post_title = get_the_title($p);
+			    $role_title = get_post_meta($p,"role-title",true);
+			    $pro_vac = get_post_meta($p,"project-vacancies",true);
+			    $post_by = get_post_field( 'post_author', $p );
+			    $pro_loc = get_post_meta($p,"project-location",true);
+			    $pro_s_date = get_post_meta($p,"project-from-date",true);
+			    $pro_e_date = get_post_meta($p,"project-to-date",true);
+			    $pro_tags = get_post_meta($p,"project-tags",true);
+			    $pro_img1 = get_post_meta($p,"project-image-1",true);
+			    $pro_img2 = get_post_meta($p,"project-image-2",true);
+			    $pro_img3 = get_post_meta($p,"project-image-3",true);
+			    $pro_img4 = get_post_meta($p,"project-image-4",true);
+			    $pro_img5 = get_post_meta($p,"project-image-5",true);
+			    $pro_des = get_post_meta($p,"project-description",true);
+			    $pro_rol = get_post_meta($p,"role-description",true);
+			    $pro_fac = get_post_meta($p,"project-facilities",false);
+			    $pro_email = get_post_meta($p,"project-email",true);
+			    $pro_phone = get_post_meta($p,"project-phone",true);
+			    $pro_mobile = get_post_meta($p,"project-mobile",true);
+			    $user_id = get_current_user_id();
+		    	if($user_id == $post_by){
+		    		$user_info = get_userdata($user_id);
+					$project = " ";
+					$project .= "<div class='header_image'>" . $pro_h_img . "</div>";
+					$project .= "<p>". $post_title .  "</p>";
+					$project .= "<p>Role Title: ". $role_title .  "</p>";
+					$project .= "<p>Vacancies: ". $pro_vac .  "</p>";
+					$project .= "<p>Posted By: " . $user_info->user_login . " Location: " . $pro_loc . "</p>";
+					$project .= "<p>Start Date: " . $pro_s_date . " Ends On: " . $pro_e_date . "</p>";
+					$project .= "<p>". $pro_tags .  "</p>";
+					$project .= "<img src=" . $pro_img1 . ">";
+					$project .= "<img src=" . $pro_img2 . ">";
+					$project .= "<img src=" . $pro_img3 . ">";
+					$project .= "<img src=" . $pro_img4 . ">";
+					$project .= "<img src=" . $pro_img5 . ">";
+					$project .= "<div class='toggle-container'>";
+					$project .= "<p><h3>Project Description</h3>". $pro_des .  "</p>";
+					$project .= "<p><h3>Role Description</h3>". $pro_rol .  "</p>";
+					$project .= "<p><h3>Facilities:</h3>";
+					$project .= "<ul>" . implode($pro_fac,', ') . "</ul></p>";
+					$project .= "<p><h3>Contact Details</h3>". $pro_email .  "</p>";
+					$project .= "<p>" . $pro_phone . " " . $pro_mobile . "</p>";
+					$project .= "</div>";
+					$project .= "<a class='moreshow'><span>Know More</span></a>";
+					//echo $project;
+					echo $project;	
 				}
-			?>
-
+			}
+				?>
 	</main><!-- .site-main -->
 
 	<?php get_sidebar( 'content-bottom' ); ?>
