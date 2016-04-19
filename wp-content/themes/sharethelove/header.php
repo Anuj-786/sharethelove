71bac266d6a6d76c6b3d871c6cdc3aa50da66b4b
KAPIL <kapil@gaiwks.(none)>
2016-04-19 14:00:26 +0530

    support us link on the footer

--- wp-content/themes/sharethelove/header.php
+++ wp-content/themes/sharethelove/header.php
@@ -51,7 +51,6 @@
                         <?php if ( is_front_page() && is_home() ) : ?>
                             <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
                         <?php else : ?>
-                            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
                         <?php endif;
 
                         $description = get_bloginfo( 'description', 'display' );
