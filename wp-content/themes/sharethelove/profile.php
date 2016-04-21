e546aab10ef71b63c917cd233634d4111f7b3bb9
ANUJ <anuj@wksanuj.(none)>
2016-04-21 16:31:14 +0530

    Anuj Changes to header and projects. NOTE: AFTER major merge conflicts.

--- wp-content/themes/sharethelove/profile.php
+++ wp-content/themes/sharethelove/profile.php
@@ -4,10 +4,7 @@ Template Name: Profile Template
 
 */
 get_header(); ?>
-
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-	<div class="secondary-menu">
+<div class="secondary-menu">
 	<?php
 	if (is_user_logged_in()) {
 	print(' <div class="secondary-menu-logo">
@@ -20,6 +17,9 @@ get_header(); ?>
 		</div>'); }
 	?>
 	</div>
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+	
      <?php  
           $home_url = home_url(); 
           $user_id = $_GET['id'];
@@ -64,26 +64,47 @@ get_header(); ?>
 		  $District34 = get_user_meta($user_id ,"District34", true); 
 		  $District35 = get_user_meta($user_id ,"District35", true); 
 
-		  $usr_distrt = $District.$District1.$District2.$District3.$District4.$District5.$District6.$District7.$District8.$District9.$District10.$District11.$District12.$District13.$District14.$District15.$District16.$District17.$District18.$District19.$District20.$District21.$District22.$District23.$District24.$District25.$District26.$District27.$District28.$District29.$District30.$$District31.$$District32.$District33.$District34.$District35;
-		  $Website = get_user_meta($user_id ,"website", true); 
-		  $Bio = get_user_meta($user_id ,"description", true); 
-		  
-		  $user_profile = " ";
-		  $user_profile .= "<div class='user-prof-img'><div class='usr-img'><img src=".$Image."></div><div class='user-prof-img-txt'>Image</div></div>";
-		  $user_profile .= "<div class='user-prof-main'>";
-		  $user_profile .= "<div class='user-prof-name'><div class='user-prof-name-txt'>Name:"." ".$Name."</div></div>";
-          $user_profile .= "<div class='user-prof-state'><div class='user-prof-state-txt'>Location:"." ".$State."(".$usr_distrt.")</div></div>";
-          $user_profile .= "<div class='user-prof-website'><div class='user-prof-website-txt'>Website:"." ".$Website."</div></div>";
-          $user_profile .= "<div class='user-prof-bio'><div class='user-prof-bio-txt'>Bio:"." ".$Bio."</div></div>";
-		  $user_profile .= "</div>";
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="secondary-menu">
+			<?php
+			if (is_user_logged_in()) {
+				print(' <div class="secondary-menu-logo">
+					<ul>
+					<li><a href="');echo home_url(); print('/activity/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/1-e1459847100362.png" title="Activity"></a></li>
+					<li><a href="');echo home_url(); print('/search/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/2-e1459847082992.png" title="search"></a></li>
+					<li><a href="');echo home_url(); print('/profile-page/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/3-e1459847059371.png" title="User Profile"></a></li>
+					<li><a href="');echo home_url(); print('/add-projects/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/4-e1459846938556.png" title="Add Project"></a></li>
+					</ul>
+					</div>'); 
+					?>
+				</div>
+				<?php  
+				$home_url = home_url(); 
+				$user_id = $_GET['id'];
+				$image = get_user_meta($user_id ,"profile_image", true);
+				$name = get_user_meta($user_id ,"first_name", true);
+				$state = get_user_meta($user_id ,"State", true); 
+				$city = get_user_meta($user_id ,"user-city", true);
+				$website = get_user_meta($user_id ,"website", true); 
+				$bio = get_user_meta($user_id ,"description", true); 
 
-		  echo $user_profile; 
-	   
-     ?>
-		
-	</main><!-- .site-main -->
+				$user_profile = " ";
+				$user_profile .= "<div class='user-prof-img'><div class='usr-img'><img src=".$Image."></div><div class='user-prof-img-txt'>Image</div></div>";
+				$user_profile .= "<div class='user-prof-main'>";
+				$user_profile .= "<div class='user-prof-name'><div class='user-prof-name-txt'>Name:"." ".$name."</div></div>";
+				$user_profile .= "<div class='user-prof-state'><div class='user-prof-state-txt'>Location:"." ".$city."(".$state.")</div></div>";
+				$user_profile .= "<div class='user-prof-website'><div class='user-prof-website-txt'>Website:"." ".$website."</div></div>";
+				$user_profile .= "<div class='user-prof-bio'><div class='user-prof-bio-txt'>Bio:</div><div class='usr-bio'>".$bio."</div></div>";
+				$user_profile .= "</div>";
 
-	
+				echo $user_profile; 
+			}
+			?>
 
-</div><!-- .content-area -->
-<?php get_footer(); ?>
+		</main><!-- .site-main -->
+
+
+
+	</div><!-- .content-area -->
+	<?php get_footer(); ?>
