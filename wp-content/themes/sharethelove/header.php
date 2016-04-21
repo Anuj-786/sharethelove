d86f05ca0fb3b32079251dc4ad089cfbd1e884e4
KAPIL <kapil@gaiwks.(none)>
2016-04-21 11:23:45 +0530

    sign up menu has been added for logged out users

--- wp-content/themes/sharethelove/header.php
+++ wp-content/themes/sharethelove/header.php
@@ -107,6 +107,7 @@
                                         print('<div><div class="login-trigger"><span>Login</span></div>');
                                         print('<div class="login-content">'); echo do_shortcode('[my-login-form]'); 
                                         print('</div></div>');
+                                        print('<a href="');echo home_url(); print('/#sign-up">Sign Up</a>');
                                         print('<a href="');echo home_url(); print('/search"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/search-home.png"></a>');
                                     }; ?>
                                 </div>
