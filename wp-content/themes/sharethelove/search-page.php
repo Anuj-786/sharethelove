8976436e7c2813581bb5d80bde528f1119a9613d
KAPIL <kapil@gaiwks.(none)>
2016-05-03 10:23:49 +0530

    changes search title for the tooltip from small s to capital S

--- wp-content/themes/sharethelove/search-page.php
+++ wp-content/themes/sharethelove/search-page.php
@@ -10,7 +10,7 @@ get_header(); ?>
 	print(' <div class="secondary-menu-logo">
 	    <ul>
 		<li><a href="');echo home_url(); print('/activity/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/1-e1459847100362.png" title="Activity"></a></li>
-		<li><a href="');echo home_url(); print('/search/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/2-e1459847082992.png" title="search"></a></li>
+		<li><a href="');echo home_url(); print('/search/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/2-e1459847082992.png" title="Search"></a></li>
 		<li><a href="');echo home_url(); print('/profile-page/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/3-e1459847059371.png" title="User Profile"></a></li>
 		<li><a href="');echo home_url(); print('/add-projects/"><img src="');echo home_url(); print('/wp-content/uploads/2016/04/4-e1459846938556.png" title="Add Project"></a></li>
 		</ul>
@@ -23,8 +23,5 @@ get_header(); ?>
 
 		<?php get_search_form(); ?>
 	</main><!-- .site-main -->
-
-	<?php get_sidebar( 'content-bottom' ); ?>
-
 </div><!-- .content-area -->
 <?php get_footer(); ?>
